51df4f7c486df2044cf90ca353c3e8c3e8298be4
JasonLy <jason.ly@gmail.com>
2017-12-18 14:28:14 -0800

    feat(): start personal stuff

---
+++ src/containers/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Home from '../components/home';
+import About from '../components/about';
+import Blog from '../components/blog';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-light">
+          <ul className="nav navbar-nav">
+            <li><Link to="/">Homes</Link></li>
+            <li><Link to="/about">about</Link></li>
+            <li><Link to="/blog">blog</Link></li>
+          </ul>
+         </nav>
+           <Route path="/" component={Home}/>
+           <Route path="/about" component={About}/>
+           <Route path="/blog" component={Blog}/>
+      </div>
+    )
+  }
+}
+
+export default App;
