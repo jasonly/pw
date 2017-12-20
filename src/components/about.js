7fcfb79f05b2c19ec80dce2b2adc1e5b440518b0
JasonLy <jason.ly@gmail.com>
2017-12-19 23:44:25 -0800

    feat(): added stuff for about

--- src/components/about.js
+++ src/components/about.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const About = () => (
   <div>
-    <h2>About</h2>
+    <p>
+      built with <a href="http://www.google.com">react</a> and <a href="http://www.google.com">aphrodite</a>
+    </p>
   </div>
 )
 
